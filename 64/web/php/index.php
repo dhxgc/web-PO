98a6e524259e412e938093b860a3b52019fbaff7
dhxgc <132298839+dhxgc@users.noreply.github.com>
2023-11-09 01:06:16 +0500

    Add files via upload

---
+++ 64/web/php/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html> 
+
+<html lang="ru">
+
+<head>
+
+   <meta charset="utf-8">
+
+   <title>
+
+       PHP
+
+   </title>
+
+   <style>
+
+       header{
+
+           border:1px solid black; 
+
+           font-size:20px; 
+
+           text-align:center;
+
+           }
+
+           .ramka{
+
+               border:1px solid black;
+
+               width: 100%;
+
+               height: 500px;
+
+           }
+
+           .kont{
+
+               width:60%;
+
+               height:500px;
+
+               float:left;
+
+               overflow: auto;
+
+               }
+
+               .kont2{
+
+               border-right: 1px solid black;
+
+               float:left;
+
+               width:300px;
+
+               height:500px;
+
+                   }
+
+           
+
+           footer{border:1px solid black; 
+
+           font-size:20px; 
+
+           text-align:center;}
+
+   </style>
+
+</head>
+
+<body>
+
+
+<header>Отчет по практике web-программирование<br>Студента группы Кс-343<br>Непомнящая Екатерина </header>
+<div class="ramka"><div class="kont2"><br><a href="index.php?str=1">Квадратное уравнение</a><br>
+<a href="index.php?str=2">Калькулятор</a><br>
+<a href="index.php?str=3">Таблица</a><br>
+<a href="index.php?str=4">Улучшенная Таблица</a><br>
+<a href="index.php?str=5">Календарь</a><br>
+<a href="index.php?str=6">Опрос</a><br>
+<a href="index.php?str=7">Чат</a><br>
+</div><div class="kont">
+</div>
+
+</div>
+ <footer>Счётчик
+
+<br>
+</footer>
+
+</body>
+
+</html>
---
+++ 64/web/php/index3.php
@@ -0,0 +1,175 @@
+
+<div>
+
+<form action="index.php?str=5" method="POST">
+
+<p>Выбрано: 8 Nov 2023</p>
+
+<button name="m" value="-1">-</button><span >
+
+ 
+
+ Nov
+   
+
+ </span><button name="m" value="1">+</button><br>
+
+<button name="g" value="-1">-</button><span >
+
+ 
+
+ 2023
+   
+
+ </span><button name="g" value="1">+</button><br>
+
+<input type=hidden name="day" value=8>
+<input type=hidden name="mes" value=11>
+<input type=hidden name="mes3" value=Nov>
+<input type=hidden name="god" value=2023>
+<input type=hidden name="god2" value=2023>
+<table class ='oh'><tr>
+<td class ="litle">Пн</td>
+<td class ="litle">Вт</td>
+<td class ="litle">Ср</td>
+<td class ="litle">Чт</td>
+<td class ="litle">Пт</td>
+<td class="red">Сб</td>
+<td class="red">Вс</td>
+</tr>
+<tr>
+<td >
+</td><td >
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=1><span
+
+class="blue">1</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=2><span
+
+class="blue">2</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=3><span
+
+class="blue">3</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=4><span
+
+class="red">4</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=5><span
+
+class="red">5</span></button>
+</td></tr><tr>
+<td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=6><span
+
+class="blue">6</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=7><span
+
+class="blue">7</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=8><span
+
+class="blue">8</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=9><span
+
+class="blue">9</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=10><span
+
+class="blue">10</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=11><span
+
+class="red">11</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=12><span
+
+class="red">12</span></button>
+</td></tr><tr>
+<td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=13><span
+
+class="blue">13</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=14><span
+
+class="blue">14</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=15><span
+
+class="blue">15</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=16><span
+
+class="blue">16</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=17><span
+
+class="blue">17</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=18><span
+
+class="red">18</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=19><span
+
+class="red">19</span></button>
+</td></tr><tr>
+<td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=20><span
+
+class="blue">20</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=21><span
+
+class="blue">21</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=22><span
+
+class="blue">22</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=23><span
+
+class="blue">23</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=24><span
+
+class="blue">24</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=25><span
+
+class="red">25</span></button>
+</td><td >
+<button style='color:red; background: transparent; border:white; text-decoration: uderline;' name="day"  value=26><span
+
+class="red">26</span></button>
+</td></tr><tr>
+<td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=27><span
+
+class="blue">27</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=28><span
+
+class="blue">28</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=29><span
+
+class="blue">29</span></button>
+</td><td >
+<button name="day" style='background: transparent; border:white; text-decoration: uderline;'  value=30><span
+
+class="blue">30</span></button>
+</td><td >
+</td><td >
+</td><td >
+</td></tr></table>
+
+<button style='background: transparent; border:white; text-decoration: uderline;' name="nows">Текущая дата</button>
+
+</form>
+</div>
